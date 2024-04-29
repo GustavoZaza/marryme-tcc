2c41ccf7ba2379f344b8f0ffa5133d40bceb3382
GustavoZaza <gustavo.buzacarini@hotmail.com>
2024-04-29 09:01:32 -0300

    inicia o projeto, e começa a criação dos components

---
+++ marryme/src/App.tsx
@@ -0,0 +1,11 @@
+import {Header} from './compenents/header'
+import './global.css'
+
+export function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
---
+++ marryme/src/components/header/index.tsx
@@ -0,0 +1,29 @@
+import './header.css'
+import logo from '../../assets/img/logo.png'
+
+export function Header() {
+    return (
+        <>
+            <div className='carrossel'>
+                <header>
+                    <div className='header-container'>
+
+                        <img className='header-container-logo' src={logo} />
+
+                        <nav>
+                            <button>Confirmar Presença</button>
+                            <button>Sobre</button>
+                            <button>Mensagem</button>
+                            <button>Presentes</button>
+                            <button>Local</button>
+                        </nav>
+
+
+
+
+                    </div>
+                </header>
+            </div>
+        </>
+    )
+}
