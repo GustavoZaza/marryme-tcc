8dfe7c9621e4cfe9b8d99023e5d4210e83933746
Gustavo Buzacarini <gustavo.buzacarini@hotmail.com>
2024-05-22 08:37:28 -0300

    inicia o about e insere novas imagens

--- marryme/src/components/about/index.tsx
+++ marryme/src/components/about/index.tsx
@@ -15,11 +15,16 @@ export function About() {
                     <h2>___________________________________</h2>
                 </div>
                 <div className="about-content">
-                    <div className="bride">
-                        <img className='bride-img' src={noiva} alt="" />
-                        <h3>Yasmin é uma designer gráfica talentosa que descobriu sua paixão pela arte desde criança. Cresceu em uma família amorosa, onde aprendeu a valorizar a criatividade e a expressão pessoal.Sua jornada a levou a explorar diferentes formas de arte, desde pintura até fotografia,antes de finalmente encontrar sua vocação no design.  Ela é uma alma livre, sempre em busca de inspiração e beleza no mundo ao seu redor.</h3>
+                    <div className="about-couple">
+                        <img src={noiva} alt="" />
+                        <div className="about-text">
+                            <p>Yasmin é uma designer gráfica talentosa que descobriu sua paixão pela arte desde criança. Cresceu em uma família amorosa, onde aprendeu a valorizar a criatividade e a expressão pessoal.Sua jornada a levou a explorar diferentes formas de arte, desde pintura até fotografia,antes de finalmente encontrar sua vocação no design.  Ela é uma alma livre, sempre em busca de inspiração e beleza no mundo ao seu redor.</p>
+                        </div>
                     </div>
-                    <div className="groom">
+                    <div className="about-couple">
+                        <div className="about-text">
+                            <p>Yasmin é uma designer gráfica talentosa que descobriu sua paixão pela arte desde criança. Cresceu em uma família amorosa, onde aprendeu a valorizar a criatividade e a expressão pessoal.Sua jornada a levou a explorar diferentes formas de arte, desde pintura até fotografia,antes de finalmente encontrar sua vocação no design.  Ela é uma alma livre, sempre em busca de inspiração e beleza no mundo ao seu redor.</p>
+                        </div>
                         <img src={noivo} alt="" />
                     </div>
                 </div>
